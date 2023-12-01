2b824ad65f12ce4ce37df718712cef0c5d7bc330
davGuzMal <dgm051195@gmail.com>
2023-12-01 13:36:37 +0100

    Home features
    Did some changes in home page appearance, to make it more attractive

--- components/lightbox/Carousel.tsx
+++ components/lightbox/Carousel.tsx
@@ -13,44 +13,36 @@ export const Carousel = () => {
 
     const updateIndex = ({ index: current }: { index: number }) => setIndex(current);
 
-  return (
-    <div className='flex space-between rounded-lg'>      
-            <div className='font-EduSA bg-gradient-to-r from-blue-100 via-gray-100 w-1/2 h-[65vh] p-4 m-4'>
-                <p className='text-6xl mt-32'>"The all is mind; the  
-                <span className='text-purple-800'> Universe</span> is mental"</p>
-                <h1 className='text-xl mt-8 text-right mr-8'>First Hermetic Principle</h1>
-            </div>           
+  return (             
 
-            <Lightbox             
-                styles={{ container: { backgroundColor: "transparent" } }}         
-                index={index}
-                slides={slides}
-                plugins={[Inline, Slideshow]}                
-                on={{
-                view: updateIndex,          
-                }}
-                carousel={{
-                padding: 12,
-                spacing: 0,
-                imageFit: "cover",
-                }}
-                slideshow={{
-                    autoplay: true,
-                    delay : 5000
-                }}
-                inline={{
-                style: {
-                    width: "50%",
-                    maxWidth: "900px",
-                    aspectRatio: "3 / 2",
-                    margin: "0 auto",
-                    // position : "absolute",                              
-                    // right : "20px"
-                },
-                }}
-            />       
-            
-        
-    </div>
+        <Lightbox             
+            styles={{ container: { backgroundColor: "transparent" } }}         
+            index={index}
+            slides={slides}
+            plugins={[Inline, Slideshow]}                
+            on={{
+            view: updateIndex,          
+            }}
+            carousel={{
+            padding: 12,
+            spacing: 0,
+            imageFit: "cover",
+            }}
+            slideshow={{
+                autoplay: true,
+                delay : 5000
+            }}
+            inline={{
+            style: {
+                width: "100%",
+                maxWidth: "1000px",
+                aspectRatio: "3 / 2",
+                margin: "0 auto",
+                // position : "absolute",                              
+                // right : "20px"
+            },
+            }}
+        />
+    
   )
 }
