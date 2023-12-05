81ae7d357683cedbb8d3cdd7834796f235070fa0
davGuzMal <dgm051195@gmail.com>
2023-12-05 18:21:12 +0100

    Erase credentials auth endpoint

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -33,61 +33,6 @@ export async function GET (request: Request){
     }
 }
 
-export async function POST(request: Request) {
-
-
-    const {
-        name,
-        email,
-        password                 
-    } = await request.json()  
-    
-    const hash = await bcrypt.hash(password, 10);    
-    try {
-        const alreadyUser = await prisma.user.findUnique({
-            where : {
-                email : email
-            }
-        })
-        if(alreadyUser) {
-            return NextResponse.json({message: "There already exist a registered user with this email, please go to LogIn page"}, { status:401} )
-        }
-        else {
-
-            if(hash){
-                const newUser = await prisma.user.create({
-                    data : {
-                        name : name,
-                        email : email,
-                        password : hash
-                    }
-                })
-                const account = await prisma.account.create({
-                    data: {
-                      userId: newUser.id,
-                      type: "credentials",
-                      provider: "credentials",
-                      providerAccountId: newUser.id,
-                    },
-                  })
-                
-                if(newUser && account){
-                    return NextResponse.json({ newUser })
-                }
-                else{
-                    return NextResponse.json({message: "Unable to link account to created user profile" }, {status: 401})
-                }
-            }
-            else{
-                return NextResponse.json({message: "It seems that something went wrong, please try again" }, {status: 401})
-            }
-        }
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({ message: "Error: " + error })
-    }
-}
-
 export async function PUT(request: Request) {
 
 
